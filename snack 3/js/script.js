8eb52aa04be456b2d4c151bb1566b48615c28647
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-12 16:36:29 +0100

    feat: add skeleton and css snack 3

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -1,2 +1,8 @@
 //Crea due tag div con due id diversi: un div avrà il testo colorato di rosso mentre l’altro di verde.
 //Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+//creo array di numeri
+
+//creo due div, uno rosso per i numeri dispari e verde per i pari e li inserisco sul DOM
+
+//creo ciclo for per smistare i numeri
