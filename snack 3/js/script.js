0d12fc3980f8372a8c0a82dac4f728b481f069ee
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-12 17:03:31 +0100

    feat: add red numbers

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -5,9 +5,30 @@
 const numbersArray = [1, 3, 4, 8, 92, 13, 66, 883, 91, 47, 109, 834, 653, 12, 41, 7, 22, 24] ;
 console.log(numbersArray , 'array')
 
+//appendo container
+const oddNumbersContainer = document.querySelector('#red-container');
+const evenNumbersContainer = document.querySelector('#green-container');
+
 //creo ciclo for per smistare i numeri, i rossi tra i dispari, i verdi tra i pari
+
+
+
 for(let i = 0; i < numbersArray.length; i++) {
+    const thisPath = numbersArray[i];
     if(numbersArray [i] % 2 === 0) {
         console.log(numbersArray[i])
+        const numberOdd = `
+        <div class="red">
+            ${thisPath}
+        </div>
+        `;
+        oddNumbersContainer.innerHTML += numberOdd;
     }
 }
+
+
+
+
+
+
+
