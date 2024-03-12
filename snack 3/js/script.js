69cafbe8db6d05a3b6091b684145fccb1c9ef7cf
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-12 16:43:32 +0100

    fix: change comments

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -3,6 +3,5 @@
 
 //creo array di numeri
 const numbersArray = [1, 3, 4, 8, 92, 13, 66, 883, 91, 47, 109, 834, 653, 12, 41, 7, 22, 24] ;
-//creo due div, uno rosso per i numeri dispari e verde per i pari e li inserisco sul DOM
 
-//creo ciclo for per smistare i numeri
+//creo ciclo for per smistare i numeri, i rossi tra i dispari, i verdi tra i pari
