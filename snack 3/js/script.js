72480421c3ad21a39866a03bf2edddf04b98d4f9
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-12 16:39:39 +0100

    add array number and css reset

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -2,7 +2,7 @@
 //Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
 //creo array di numeri
-
+const numbersArray = [1, 3, 4, 8, 92, 13, 66, 883, 91, 47, 109, 834, 653, 12, 41, 7, 22, 24] ;
 //creo due div, uno rosso per i numeri dispari e verde per i pari e li inserisco sul DOM
 
 //creo ciclo for per smistare i numeri
