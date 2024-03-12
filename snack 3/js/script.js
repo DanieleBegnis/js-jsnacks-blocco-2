48e51fac1923d01f7ea2131c3f0f2c93345f015d
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-12 16:51:36 +0100

    feat: add console log even numbers

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -3,5 +3,11 @@
 
 //creo array di numeri
 const numbersArray = [1, 3, 4, 8, 92, 13, 66, 883, 91, 47, 109, 834, 653, 12, 41, 7, 22, 24] ;
+console.log(numbersArray , 'array')
 
 //creo ciclo for per smistare i numeri, i rossi tra i dispari, i verdi tra i pari
+for(let i = 0; i < numbersArray.length; i++) {
+    if(numbersArray [i] % 2 === 0) {
+        console.log(numbersArray[i])
+    }
+}
