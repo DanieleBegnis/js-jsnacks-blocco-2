e5a1929338fe5d601d3ec50a0e6f124960b219b3
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-12 16:28:15 +0100

    feat: add second snack

--- snack 2/js/script.js
+++ snack 2/js/script.js
@@ -1,1 +1,10 @@
 //L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+//chiedo all'utente due parole
+const userFirstWord = prompt('Dimmi una parola');
+const userSecondWord = prompt('Dimmi una seconda parola');
+
+if(userFirstWord.length > userSecondWord.length) {
+    console.log(userFirstWord);
+} else {
+    console.log(userSecondWord);
+}
